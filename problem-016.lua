1fad137212729dbb2e8371affea297c1c5d05160
rekky1aws <killikomfr@gmail.com>
2025-06-26 08:35:38 +0200

    Fixed Typo

--- problem-016.lua
+++ problem-016.lua
@@ -12,5 +12,5 @@ print(string.len(text))
 -- print(sum)
 
 -- Apparently the sum is not correct
--- It seems there is the correct number of digits (since ceil(log(2¹⁰⁰⁰)) = 302)
+-- It seems there is the correct number of digits (since ceil(log(2^1000)) = 302)
 -- There might be an error when summing all the digits
