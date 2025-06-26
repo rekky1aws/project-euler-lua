0a6d7f323566daac631569a750bdc8dba50b3cc3
rekky1aws <killikomfr@gmail.com>
2025-06-26 08:42:01 +0200

    Problem 16

--- problem-016.lua
+++ problem-016.lua
@@ -5,12 +5,6 @@ sum = 0
 for i=1,string.len(text) do
    toadd = string.sub(text,i,i)
    sum = sum + tonumber(toadd)
-   print(i)
 end
 
-print(string.len(text))
--- print(sum)
-
--- Apparently the sum is not correct
--- It seems there is the correct number of digits (since ceil(log(2^1000)) = 302)
--- There might be an error when summing all the digits
+print("result :", sum)
