4caea25e85bc531a1b77076a0f81ab7a9e0ee8ae
rekky1aws <killikomfr@gmail.com>
2025-06-27 21:43:45 +0200

    WIP 17
    Working function to output text version of a number beetween 1 and 1099.

--- problem-017.lua
+++ problem-017.lua
@@ -45,5 +45,51 @@ function spell_number (nb)
 		"thousand"
 	}
 
+	-- XXXXX__ 
+	if nb > 100 then
+		nb_div_100 = nb // 100
+		dec = 1
 
+		while nb_div_100 > 10 do
+			result = result .. words[nb_div_100%10]
+			result = result .. words_3[dec]
+			nb_mod_100 = nb_mod_100 // 10
+			dec = dec + 1
+		end
+
+		if nb_div_100 ~= 10 then
+			result = result .. words[nb_div_100]
+			result = result .. words_3[dec]
+		else
+			result = result .. words_3[dec+1]
+		end
+
+		if nb % 100  ~= 0 then
+			result =  result .. "and"
+		end	
+	end
+
+	
+	-- ______XX
+	nb_mod_100 = nb % 100
+	if nb_mod_100 < 20 then
+		if nb_mod_100 > 0 then
+			result = result .. tostring(words[nb_mod_100])
+		end
+	else
+		if nb_mod_100 % 10 == 0 then
+			result = result .. tostring(words_2[(nb_mod_100//10) - 1])
+		else
+			result = result .. tostring(words_2[(nb_mod_100//10) - 1])
+			result = result .. tostring(words[nb_mod_100%10])
+		end
+	end
+
+	
+	return result
+end
+
+for i=1,1000 do
+	spell = spell_number(i)
+	print(i, spell)
 end
