d28a1aa9eb3fa5a196821decda8e20d22f23aee1
rekky1aws <killikomfr@gmail.com>
2025-07-01 02:35:49 +0200

    WIP 18
    + Function to display correctly the pyramid in terminal

--- problem-018.lua
+++ problem-018.lua
@@ -15,3 +15,17 @@ pyramid = {
 	{63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
 	{04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23}
 }
+
+function disp_pyr (pyramid)
+	for i=1,#(pyramid) do
+		for k=1,(#(pyramid) - i) do
+			io.write('  ')
+		end
+		for j=1,#(pyramid[i]) do
+			io.write(string.format("%02d  ", pyramid[i][j]))
+		end
+		io.write("\n")
+	end
+end
+
+disp_pyr(pyramid)
