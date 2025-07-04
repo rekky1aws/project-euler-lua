1bf03ea81978afb104104beb840ab40400b3c99b
rekky1aws <killikomfr@gmail.com>
2025-07-05 00:16:29 +0200

    Pruned problem-018.lua from unused code.

--- problem-018.lua
+++ problem-018.lua
@@ -16,62 +16,8 @@ pyramid = {
 	{04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23}
 }
 
-function disp_pyr (pyramid)
-	for i=1,#(pyramid) do
-		for k=1,(#(pyramid) - i) do
-			io.write('  ')
-		end
-		for j=1,#(pyramid[i]) do
-			io.write(string.format("%02d  ", pyramid[i][j]))
-		end
-		io.write("\n")
-	end
-end
-
-function hghst_nb_pos (pyramid)
-	hghst_val = 0
-	hghst_i = 0
-	hghst_j = 0
-
-	for i=1,#(pyramid) do
-		for j=1,#(pyramid[i]) do
-			if pyramid[i][j] > hghst_val then
-				hghst_val = pyramid[i][j]
-				hghst_i = i
-				hghst_j = j
-			end
-		end
-	end
-
-	result = {hghst_i, hghst_j}
-
-	return result
-end
-
-function path (pyramid, total, i, j)
-	-- end of the pyramid
-	if i == #(pyramid) then
-		return total
-	end
-
-	-- start of the pyramid
-	if #(pyramid[i]) == 1 then
-		total = pyramid[i][1]
-		total = total + path(pyramid, total, i+1, j)
-	end
-
-	if #(pyramid[i]) then
-		if pyramid[i][j] > pyramid[i][j] then
-			total = total + path(pyramid, total, i+1, j)
-		else
-			total = total + path(pyramid, total, i+1, j+1)
-		end
-	end 
-end
-
 function dec_to_bin(num)
-    -- returns a table of bits, least significant first.
-    local t={} -- will contain the bits
+    local t={}
     while num>0 do
         rest=math.fmod(num,2)
         t[#t+1]=rest
@@ -88,18 +34,11 @@ function path_sum (pathing, pyramid)
 		if pathing[i-1] then
 			j = j + pathing[i-1]
 		end
-		-- print(j)
 		result = result + pyramid[i][j]
 	end
-	-- print("====")
 
 	return result
 end
-
--- disp_pyr(pyramid)
-
---high_coords = hghst_nb_pos(pyramid)
---print(string.format("The highest value is %d at i=%d and j=%d", pyramid[high_coords[1]][high_coords[2]], high_coords[1], high_coords[2]))
 
 max = 0
 for i=0,2^(#(pyramid)-1)-1 do
